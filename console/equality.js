8bce7138aab62d0e930c07ccc63b5df336e5cf3b
Kotireddy Kamireddy <kotireddyk0485@gmail.com>
2022-12-20 16:15:56 +0530

    basics checking

---
+++ console/equality.js
@@ -0,0 +1,7 @@
+let num = 013;
+console.log(num == 13) // output :- false
+
+console.log(num);//output :- 11
+
+//convert octal to  decimal string
+console.log(num.toString(8) == 13) // output:- true
